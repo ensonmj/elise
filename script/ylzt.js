75eb69baea00ef3d9ab5fdc1d0027bf07a975128
ensonmj <ensonmj@gmail.com>
2016-09-09 16:26:32 +0800

    first work revision with simple example

---
+++ script/ylzt.js
@@ -0,0 +1,16 @@
+var result = {};
+
+result['product_url_0'] = product_url_0;
+result['product_name_0'] = document.querySelector('head > title').innerText;
+result['product_img_0'] = window.getComputedStyle(document.querySelector('.page1')).backgroundImage.split(/\(|\)/)[1];
+
+var imgs = document.querySelectorAll('.ts_img > img');
+if (imgs) {
+  [].forEach.call(imgs, function(item, indexY) {
+    try {
+      result['product_img'+indexY+'_0'] = item.src;
+    } catch (error) {}
+  });
+}
+
+return result;
